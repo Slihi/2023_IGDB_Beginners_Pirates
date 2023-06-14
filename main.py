6dca64e40d4dbad71e4d997891347d885b668bca
dsdude3021 <dsdude3021@gmail.com>
2023-06-14 11:46:41 -0400

    Basic Window

--- main.py
+++ main.py
@@ -3,3 +3,32 @@ import numpy as np
 from pyglet.window import key
 
 #TEST GIT PUSH
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0 , 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+#Variables
+ScreenSize = (800, 600)
+game_title = "Game"
+FPS = 60
+
+#Window
+window = pyglet.window.Window(ScreenSize[0], ScreenSize[1], "Game")
+
+#Events
+@window.event
+def on_draw():
+    window.clear
+
+@window.event
+def update(dt):
+    pass
+
+#Run
+if __name__ == "__main__":
+    pyglet.clock.schedule_interval(update, 1 / FPS)
+    pyglet.app.run()

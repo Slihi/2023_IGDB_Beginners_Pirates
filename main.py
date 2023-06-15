00d85e2cac664042b369fde93e7c231fe8725083
dsdude3021 <dsdude3021@gmail.com>
2023-06-15 00:38:57 -0400

    Testing with assets and transparencies

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ pygl_assets = pyglet.resource.path = ['Assets']
 pyglet.resource.reindex()
 X_Mark_Image = pyglet.resource.image("X_Mark.png")
 ShipLv1_Image = pyglet.resource.image("ShipLv1.png")
+Ship_bigger = pyglet.resource.image("Ship_bigger.png")
+ShipLV1_JPEG = pyglet.resource.image("ShipLv1.jpeg")
 
 #Colors
 WHITE = (255, 255, 255)
@@ -102,12 +104,14 @@ def on_draw():
     #square = pyglet.shapes.Rectangle(0, 0, 50, 100, color=TAN)
     #square.draw()
 
-    #Curser/ X_Mark
-    X_Mark.draw()
     #Scale Ship
-    ShipLv1_Image.width = 34 * 2
-    ShipLv1_Image.height = 64 * 2
-    ShipLv1_Image.blit(0, 0)
+    #ShipLv1_Image.width = 34 * 2
+    #ShipLv1_Image.height = 64 * 2
+    #ShipLv1_Image.blit(0, 0)
+    ShipLV1_JPEG.blit(0, 0)
+
+    # Curser/ X_Mark
+    X_Mark.draw()
 
 @window.event
 def update(dt):
